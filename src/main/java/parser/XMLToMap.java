0f898db9c6507c38b40f3d16b428e4f5335507da
김민섭 <sublivan29@gmail.com>
2022-05-24 23:54:12 +0900

    update

--- src/main/java/parser/XMLToMap.java
+++ src/main/java/parser/XMLToMap.java
@@ -2,9 +2,12 @@ package parser;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import org.jdom2.Content;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
@@ -13,31 +16,86 @@ import org.jdom2.input.SAXBuilder;
 public class XMLToMap {
 
     public static void main(String[] args) throws IOException, JDOMException {
-        // 1. XML 로드
-        // 1-1.문자열 파싱 시
-//        String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?> <catalog> <book id=\"bk101\"> "
-//            + "<author>Gambardella, Matthew</author> <title>XML Developer's Guide</title> "
-//            + "<genre>Computer</genre> <price>44.95</price> <publish_date>2000-10-01</publish_date> "
-//            + "<description>An in-depth look at creating applications with XML.</description> </book> "
-//            + "<book id=\"bk102\"> <author>Ralls, Kim</author> <title>Midnight Rain</title> "
-//            + "<genre>Fantasy</genre> <price>5.95</price> <publish_date>2000-12-16</publish_date> "
-//            + "<description>A former architect battles corporate zombies, an evil sorceress, and her own childhood to become queen of the world.</description> "
-//            + "</book> </catalog>";
 
-//        Document document = new SAXBuilder().build(new StringReader(xml));
+        System.out.println("방법 1.");
 
-        // 1-2. 파일 파싱 시
-        Document document = new SAXBuilder().build(new File("C:/GitHub/JavaCodeExample/src/main/java/parser/sample2.xml"));
+        // XML 로드
+        // 문자열 파싱 시
+        String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?> <catalog> <book id=\"bk101\"> "
+            + "<author>Gambardella, Matthew</author> <title>XML Developer's Guide</title> "
+            + "<genre>Computer</genre> <price>44.95</price> <publish_date>2000-10-01</publish_date> "
+            + "<description>An in-depth look at creating applications with XML.</description> </book> "
+            + "<book id=\"bk102\"> <author>Ralls, Kim</author> <title>Midnight Rain</title> "
+            + "<genre>Fantasy</genre> <price>5.95</price> <publish_date>2000-12-16</publish_date> "
+            + "<description>A former architect battles corporate zombies, an evil sorceress, and her own childhood to become queen of the world.</description> "
+            + "</book> </catalog>";
 
-        // 2. Root Element (catalog)
-        Element rootElement = document.getRootElement();
+        // 파일 파싱
+        Document document1 = new SAXBuilder().build(new StringReader(xml));
 
-        // 3. Root Element (book)
+        // Root Element (catalog)
+        Element rootElement1 = document1.getRootElement();
+
+        xmlToMap2(rootElement1);
+
+        System.out.println("\n");
+
+
+        System.out.println("방법 2.");
+        // XML 로드
+        // 파일 파싱
+        Document document2 = new SAXBuilder().build(new File("C:/GitHub/JavaCodeExample/src/main/java/parser/sample2.xml"));
+
+        // Root Element (catalog)
+        Element rootElement2 = document2.getRootElement();
+
+        Map<String, Object> resMap = new HashMap<>();
+        xmlToMap(rootElement2, resMap);
+
+        for (String name : resMap.keySet()){
+            System.out.println("name = " + name + " : " +  "value = " + resMap.get(name));
+        }
+    }
+
+    public static void xmlToMap(Element element, Map<String, Object> map) {
+        List<?> childList = element.getChildren();
+        Iterator<?> childItr = childList.iterator();
+
+        while(childItr.hasNext()) {
+            Element childEl = (Element)childItr.next();
+            String elName = childEl.getName();
+            List<?> grandChildList = childEl.getChildren();
+            Iterator<?> grandChildItr = grandChildList.iterator();
+
+            if (grandChildItr.hasNext()) {
+                xmlToMap(childEl, map);
+
+            } else {
+                List<?> contentList = childEl.getContent();
+                Iterator<?> contentItr = contentList.iterator();
+
+                if (contentItr.hasNext()) {
+                    Content content = (Content) contentItr.next();
+
+                    map.put(elName, content.getValue());
+
+                } else {
+                    System.out.println(elName + " : Null value");
+                }
+            }
+
+        }
+    }
+
+    public static void xmlToMap2(Element rootElement) {
+
+        // Root Element (book)
         List<Element> bookElements = rootElement.getChildren();
 
         Map<String, Object> resMap = new HashMap<>();
 
         for(Element bookElement : bookElements){
+            Map<String, Object> tempMap = new HashMap<>();
 
             String bookAttributeId = bookElement.getAttributeValue("id");
             System.out.println("==== ==== ==== ==== ====");
@@ -50,9 +108,9 @@ public class XMLToMap {
                 String value = bookChildrenElement.getValue();
                 System.out.println(name + " : " + value);
 
-                resMap.put(name, value);
+                tempMap.put(name, value);
             }
-
+            resMap.put(bookAttributeId, tempMap);
             System.out.println("==== ==== ==== ==== ====");
         }
 
@@ -63,6 +121,7 @@ public class XMLToMap {
         }
         System.out.println("==== ==== ==== ==== ====");
     }
+
 }
 
 /*
