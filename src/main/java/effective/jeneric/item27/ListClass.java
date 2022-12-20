8290af1a6850cc944364d5ff81d775b20effa008
sublivan <sublivan29@gmail.com>
2022-12-21 00:23:06 +0900

    [Effective Java] Item27. 비검사 경고를 제거하라

---
+++ src/main/java/effective/jeneric/item27/ListClass.java
@@ -0,0 +1,36 @@
+package effective.jeneric.item27;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ListClass<T> {
+    private int size;
+    private T[] elements;
+
+    public <T> T[] toArray(T[] a) {
+        if (a.length < size) {
+            // 생성한 배열과 매개변수로 받은 배열의 타입이 모두 T[]로 같으므로
+            // 올바른 형변환
+            @SuppressWarnings("unchecked")
+            T[] result = (T[]) Arrays.copyOf(elements, size, a.getClass());
+            return result;
+        }
+        System.arraycopy(elements, 0, a, 0, size);
+        if (a.length > size)
+            a[size] = null;
+        return a;
+    }
+
+    /** ArrayList.toArray()*/
+//    @SuppressWarnings("unchecked")
+//    public <T> T[] toArray(T[] a) {
+//        if (a.length < size)
+//            // Make a new array of a's runtime type, but my contensts:
+//            return (T[]) Arrays.copyOf(elementData, size, a.getClass());
+//        System.arraycopy(elementData, 0, a, 0, size);
+//        if (a.length > size)
+//            a[size] = null;
+//        return a;
+//    }
+}
